28a9a5f7c9075d2e66b999f10f7201bdc361a374
imf4isal <mdfaisal.dev@gmail.com>
2024-08-25 10:32:36 +0600

    hide topbar unnecessar stuff

--- popup.js
+++ popup.js
@@ -5,54 +5,109 @@ async function injectStyle(tabId, file) {
     });
 }
 
-// initital state
-chrome.storage.local.get(['state'], async function ({ state }) {
-    const toggleSwitch = document.getElementById('toggleSwitch');
+async function removeStyle(tabId, file) {
+    await chrome.scripting.removeCSS({
+        files: [file],
+        target: { tabId },
+    });
+}
 
-    if (!state || state === 'OFF') {
-        chrome.storage.local.set({ state: 'ON' });
+// Initial state
+chrome.storage.local.get(
+    ['state', 'fullZenState'],
+    async function ({ state, fullZenState }) {
+        const toggleSwitch = document.getElementById('toggleSwitch');
+        const fullZenSwitch = document.getElementById('fullZenSwitch');
 
-        toggleSwitch.checked = true;
+        if (!state || state === 'OFF') {
+            chrome.storage.local.set({ state: 'ON' });
+            toggleSwitch.checked = true;
 
-        const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+            const [tab] = await chrome.tabs.query({
+                active: true,
+                currentWindow: true,
+            });
+            const { url, id: tabId } = tab;
+
+            const extensionURL = 'youtube.com';
+            if (url.includes(extensionURL)) {
+                injectStyle(tabId, 'zen.css');
+            }
+
+            updateBadgeText(tabId, 'ON');
+        } else {
+            toggleSwitch.checked = true;
+        }
+
+        // Set the fullZenSwitch state based on stored value
+        if (fullZenState === 'ON') {
+            fullZenSwitch.checked = true;
+
+            const [tab] = await chrome.tabs.query({
+                active: true,
+                currentWindow: true,
+            });
+            const { url, id: tabId } = tab;
+
+            const extensionURL = 'youtube.com';
+            if (url.includes(extensionURL)) {
+                injectStyle(tabId, 'zenFull.css');
+            }
+        }
+    }
+);
+
+// Toggle for the main Zen Mode switch
+document
+    .getElementById('toggleSwitch')
+    .addEventListener('change', async (event) => {
+        const [tab] = await chrome.tabs.query({
+            active: true,
+            currentWindow: true,
+        });
         const { url, id: tabId } = tab;
 
         const extensionURL = 'youtube.com';
         if (url.includes(extensionURL)) {
-            injectStyle(tabId, 'zen.css');
+            const isChecked = event.target.checked;
+            const nextState = isChecked ? 'ON' : 'OFF';
+
+            if (nextState === 'ON') injectStyle(tabId, 'zen.css');
+            else {
+                injectStyle(tabId, 'zenOff.css');
+                removeStyle(tabId, 'zenFull.css');
+                chrome.storage.local.set({ fullZenState: 'OFF' });
+                document.getElementById('fullZenSwitch').checked = false;
+            }
+
+            chrome.storage.local.set({ state: nextState });
+            updateBadgeText(tabId, nextState);
         }
-
-        updateBadgeText(tabId, 'ON');
-    } else {
-        toggleSwitch.checked = true;
-    }
-});
-
-document.getElementById('toggleSwitch').addEventListener('change', async (event) => {
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    const { url, id: tabId } = tab;
-
-    const extensionURL = 'youtube.com';
-    if (url.includes(extensionURL)) {
-        const isChecked = event.target.checked;
-        const nextState = isChecked ? 'ON' : 'OFF';
-
-        if (nextState === 'ON') injectStyle(tabId, 'zen.css');
-        else injectStyle(tabId, 'zenOff.css');
-
-        chrome.storage.local.set({ state: nextState });
-        updateBadgeText(tabId, nextState);
-    }
-});
-
-function getState() {
-    return new Promise((resolve) => {
-        chrome.storage.local.get(['state'], function ({ state }) {
-            resolve(state);
-        });
     });
-}
 
+// recommendations toggle
+document
+    .getElementById('fullZenSwitch')
+    .addEventListener('change', async (event) => {
+        const [tab] = await chrome.tabs.query({
+            active: true,
+            currentWindow: true,
+        });
+        const { url, id: tabId } = tab;
+
+        const extensionURL = 'youtube.com';
+        if (url.includes(extensionURL)) {
+            const isChecked = event.target.checked;
+            const fullZenState = isChecked ? 'ON' : 'OFF';
+
+            if (fullZenState === 'ON') injectStyle(tabId, 'zenFull.css');
+            else removeStyle(tabId, 'zenFull.css');
+
+            chrome.storage.local.set({ fullZenState });
+        }
+    });
+
+// update badge text
 function updateBadgeText(tabId, text) {
     chrome.action.setBadgeText({
         tabId,
